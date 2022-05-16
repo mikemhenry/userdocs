fa4e311479ba4e38af1bff49afdd54be7027855b
Jamie Cheong <cheongj1@mskcc.org>
2022-05-16 11:42:00 -0400

    Update publish code

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@
 
 git clone git@github.mskcc.org:HPC/userdocs.git gh-pages
 cd gh-pages
-mkdocs gh-deploy --config-file ../mkdocs.yml --remote-branch master
+mkdocs gh-deploy --config-file ../mkdocs.yml --remote-branch main
 cd ..
 
 exit 0
